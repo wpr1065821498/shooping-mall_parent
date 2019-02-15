2232fe30a4cf8f86e1250e7ea6906186dbddf809
wpr1065821498 <1065821498@qq.com>
2019-02-15 19:59:18 +0800

    修改的代码

--- plat_parent/plat_server_8001/src/main/java/provide/controller/LoginController.java
+++ plat_parent/plat_server_8001/src/main/java/provide/controller/LoginController.java
@@ -10,6 +10,10 @@ import until.AjaxResult;
 @RestController
 @RequestMapping("/index")
 public class LoginController {
+    //@requestBody这个注解的作用是将前台传过来的restful风格请求中的json参数直接封装成一个domain对象
+    //前提是json的属性名称必须个domain的名称相匹配
+    //method的作用是用来
+    //restful请求是使用url来定位资源，所以如下代码要写成@RequestMapping(value = "/login",method = RequestMethod.POST)
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public AjaxResult getAjax(@RequestBody Employee employee){
         if("admin".equals(employee.getUsername()) && "0".equals(employee.getPassword())){
